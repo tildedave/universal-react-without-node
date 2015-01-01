9bee313e089693a263601927be8b54740d256207
Dave King <tildedave@gmail.com>
2015-01-01 10:26:17 -0800

    Use webpack instead

--- test.js
+++ test.js
@@ -1,4 +1,17 @@
-var React = global.React;
+if (typeof(window) === 'undefined') {
+  var global = {};
+
+  function consoleShim() {
+    print(Array.prototype.join.call(arguments, ' '));
+  }
+
+  var console = {};
+  ['debug', 'log', 'info', 'warn'].forEach(function(i) {
+    console[i] = consoleShim;
+  });
+}
+
+var React = require('react');
 
 var A = React.createClass({
   render: function() {
